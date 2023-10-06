54531a85d2b81a46ead83a9d4931dc641cc8c35e
David Reina <davidereina@hotmail.com>
2023-10-06 09:38:04 -0500

    Simple program to fill an array with friend names and iterate through it in order to send greeting to them

---
+++ ArrayExercise/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ArrayExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] friends = new string[5];
+
+            for (int i = 0; i < friends.Length; i++)
+            {
+                Console.WriteLine("Add a new friend");
+                string input = Console.ReadLine();
+                friends[i] = input;
+            }
+
+            Console.WriteLine("\n");
+
+            foreach(string friend in friends)
+            {
+                Console.WriteLine($"Greetings to {friend}");
+            }
+        }
+    }
+}
